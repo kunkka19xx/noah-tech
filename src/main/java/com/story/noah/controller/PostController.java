2c21d18b3e21a0947fac0340cc4c36ca0dc81332
kunkka19xx <53131553+kunkka19xx@users.noreply.github.com>
2024-01-03 16:39:41 +0900

    add render pg db

--- src/main/java/com/story/noah/controller/PostController.java
+++ src/main/java/com/story/noah/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.beans.BeanProperty;
--- src/main/java/com/story/noah/model/Category.java
+++ src/main/java/com/story/noah/model/Category.java
@@ -1,10 +1,12 @@
 package com.story.noah.model;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDateTime;
 import java.time.OffsetTime;
